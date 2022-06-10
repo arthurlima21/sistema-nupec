b1ab3c528f2520211f70c2f4b9f2c799b0f2ccdf
arthurlimaaf <arthurf.lima21@gmail.com>
2022-06-10 16:21:38 -0400

    commit

--- src/components/Escolas/Consultar_Escola.js
+++ src/components/Escolas/Consultar_Escola.js
@@ -1,26 +1,103 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import api from "../../api/api";
+import { Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import Image from 'react-bootstrap/Image';
+import { Table } from "react-bootstrap";
+import { useHistory } from "react-router";
+import { Dropdown } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
-function App() {
-  const [fetchedData, setFetchedData] = useState([]);
-  useEffect(() => {
-    const getData = async () => {
-      const data = await api.get(
-        "/listEscola"
-      );
-      setFetchedData(data);
-    };
-    getData();
-  }, []);
+function Consultar_Escola() {
 
-  console.log("data: ", fetchedData);
+    const [consultEscola, setConsultEscola] = React.useState(null);
+    const history = useHistory();
 
-  return (
-    <div className="App">
-      <h1>test</h1>
-      {fetchedData.data ? <h2>name: {fetchedData.data.name}</h2> : null}
-    </div>
-  );
+    // const updatePack = (pack_id, status, qtd_devices) => {
+    //     history.push("/update-pack", { pack_id, status, qtd_devices });
+
+    // }
+
+    // const deviceList = (pack_id) => {
+    //     history.push("/device-list", {pack_id});
+    // }
+
+    function Home() {
+        history.push('/index');
+    }
+
+    React.useEffect(() => {
+        api.get("https://api.adviceslip.com/advice").then((response) => {
+          setConsultEscola(response.data);
+        });
+        
+      }, []);
+    
+    if (!consultEscola) return null;  
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+            <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="/maps.png"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        SISTEMA NUPEC
+                    </Navbar.Brand>
+                </Navbar>
+
+                <Container>
+                    {/* <Row className="justify-content-md-center">
+                        <Image src="logo.png" rounded style={{ width: '10rem' }} />
+                    </Row> */}
+                    <Row className="justify-content-md-center">
+                        <h3>CONSULTAR ESCOLA</h3>
+                    </Row>
+                </Container>
+
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Table striped bordered hover variant="dark" >
+                            <thead>
+                                <tr>
+                                    <th>CODIGO ESCOLA</th>
+                                    <th>NOME ESCOLA</th>
+                                </tr>
+                            </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>{consultEscola.slip.id}</td>
+                                            <td>{consultEscola.slip.advice}</td>
+                                            {/* <Button onClick={deviceList} variant="success">List Devices</Button> */}
+                                            <Dropdown>
+                                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                                    OPÇÕES
+                                                </Dropdown.Toggle>
+
+                                                <Dropdown.Menu>
+                                                    <Dropdown.Item>Atualizar</Dropdown.Item>
+                                                    <Dropdown.Item>Deletar</Dropdown.Item>
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+                                        </tr>
+                                    </tbody>
+
+                        </Table>
+                    </Row>
+                </Container>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default Consultar_Escola;
+
+
+
+
